505db43ef4474ecfb3411c36c2e1cb7a376b70af
Alex Mendes Queiroz <alexqueirozmendes@gmail.com>
2026-07-10 09:27:12 -0300

    feat(form): adiciona a logica de enviar o conteudo dos inputas para o App.tsx e adiciona alguns comentarios

--- ijunior-task-board/src/components/NewServiceForm.tsx
+++ ijunior-task-board/src/components/NewServiceForm.tsx
@@ -1,29 +1,41 @@
 import React, { useState } from "react"
 
-export function NewServiceForm() {
-    const [formData, setFormData] = useState({ nomeCliente: '', modeloAparelho: '', defeito: '' });
+interface NewServiceFormProps {
+    onAddService: (data: {
+        nomeCliente: string;
+        modeloAparelho: string;
+        defeito: string;
+    }) => void;
+}
+
+export function NewServiceForm({ onAddService }: NewServiceFormProps) {
+    const [form, setForm] = useState({ nomeCliente: '', modeloAparelho: '', defeito: '' });
 
     const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+        // prevenindo que a pagina recarregue automaticamente apos clicar no botao
         e.preventDefault();
 
+        // confetindo se todos os campos estao preenchidos
         if(!e.currentTarget.checkValidity()) {
             return;
         }
 
-        console.log(formData);
+        // enviando os valores dos inputs do form para a componente pai (App.tsx)
+        onAddService(form);
 
-        setFormData({ nomeCliente: '', modeloAparelho: '', defeito: ''})
+        // limpando os campos dos inputs
+        setForm({ nomeCliente: '', modeloAparelho: '', defeito: ''})
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit} className="bg-white">
                 <input type="text" required name="nomeCliente" placeholder="Nome do Cliente" 
-                value={formData.nomeCliente} onChange={(e) => setFormData({...formData, nomeCliente: e.target.value})}/>
+                value={form.nomeCliente} onChange={(e) => setForm({...form, nomeCliente: e.target.value})}/>
                 <input type="text" required name="modeloAparelho" placeholder="Modelo do Aparelho" 
-                value={formData.modeloAparelho} onChange={(e) => setFormData({...formData, modeloAparelho: e.target.value})}/>
+                value={form.modeloAparelho} onChange={(e) => setForm({...form, modeloAparelho: e.target.value})}/>
                 <input type="text" required name="defeito" placeholder="Defeito apresentado" 
-                value={formData.defeito} onChange={(e) => setFormData({...formData, defeito: e.target.value})}/>
+                value={form.defeito} onChange={(e) => setForm({...form, defeito: e.target.value})}/>
                 <input type="submit" value={`Salvar`}/>
             </form>
         </div>
