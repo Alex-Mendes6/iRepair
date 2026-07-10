16d14cdaf096e134431572096f24b5d755ff3d74
Alex Mendes Queiroz <alexqueirozmendes@gmail.com>
2026-07-10 09:53:01 -0300

    feat(App): adiciona a criacao dinamica de OS atraves dos inputs do form

--- ijunior-task-board/src/App.tsx
+++ ijunior-task-board/src/App.tsx
@@ -1,20 +1,40 @@
+import { useState } from 'react'
 import './App.css'
 import { Header } from './components/Header'
 import { ServiceCard } from './components/ServiceCard'
 import { NewServiceForm } from './components/NewServiceForm'
 
 function App() {
+  interface Servico {
+  nomeCliente: string;
+  modeloAparelho: string;
+  defeito: string;
+}
+
+  const [servicos, setServicos] = useState<Servico[]>([]);
+
+  const handleAddServicos = (novoServico: Servico) => {
+    setServicos([...servicos, novoServico]);
+  };
+
   return (
     <div>
       <div>
         <Header />
       </div>
       <main>
-        <div className='gap-4 flex'>
-            <ServiceCard nomeCliente='Alex' modeloAparelho='TV' defeito='Tela Preta'/>
-        </div>
         <div>
-            <NewServiceForm />
+            <NewServiceForm onAddService={handleAddServicos}/>
+        </div>
+        <div className='gap-4 flex'>
+            {servicos.map((servico, index) => (
+              <ServiceCard 
+              key={index}
+              nomeCliente={servico.nomeCliente}
+              modeloAparelho={servico.modeloAparelho}
+              defeito={servico.defeito}
+              />
+            ))}
         </div>
       </main>
     </div>
