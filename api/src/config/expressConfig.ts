3eebeed11d24af3a52cc94a5890c0efe3ae682b9
Alex Mendes Queiroz <alexqueirozmendes@gmail.com>
2026-08-04 20:22:39 -0300

    feat(cookie): adiciona o uso do cookie parser no app

--- api/src/config/expressConfig.ts
+++ api/src/config/expressConfig.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { tarefaRoutes } from '../routes/tarefa.routes';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use('/tasks', tarefaRoutes);
 
 export { app };
