9b8138cbae92dc08f5e4a9a96d8c16e9318adb35
Alex Mendes Queiroz <alexqueirozmendes@gmail.com>
2026-08-11 08:57:36 -0300

    feat(integracao): adiciona o cors que une o back-end com o front-end

--- api/src/config/expressConfig.ts
+++ api/src/config/expressConfig.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { tarefaRoutes } from '../domains/tarefas/routes/tarefa.routes';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import { authRoutes } from '../domains/auth/auth.routes';
 import { clienteRoutes } from '../domains/clientes/routes/clients.routes';
 import { serviceOrderRoutes } from '../domains/serviceorders/routes/serviceorders.routes';
@@ -8,6 +9,12 @@ import { serviceOrderRoutes } from '../domains/serviceorders/routes/serviceorder
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: 'http//localhost:5173',
+    credentials: true,
+}));
+
+// Rotas
 app.use('/tasks', tarefaRoutes);
 app.use('/auth', authRoutes);
 app.use('/clients', clienteRoutes);
